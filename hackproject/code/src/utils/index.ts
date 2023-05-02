3c5eb6d4e82cfdbcddf0dc9a16403b9692948707
Swithin Annor Okyere <jayswithin@gmail.com>
2023-05-02 18:15:52 +0000

    feat: implement accessibility validator for contrast (#4)
    Co-authored-by: Swithin Okyere <swithin.okyere@turntabl.io>

--- hackproject/code/src/utils/index.ts
+++ hackproject/code/src/utils/index.ts
@@ -1,4 +1,4 @@
 export { default as file } from "./file";
 export { default as srcPath } from "./srcPath";
-export { default as parseCss } from "./parseCss";
-export { default as rgbToHex } from "./rgbToHex";
+export { default as rgbToHex, DEFAULT_BACKGROUND, DEFAULT_FOREGROUND } from "./color";
+export { validate } from "./contrast";
