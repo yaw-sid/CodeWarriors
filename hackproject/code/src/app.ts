442d238413bda6a36f52c1a091a2e4470e3aef5e
BobBy <ayiteybobby@gmail.com>
2023-05-03 15:31:17 +0000

    Get all css in html link tags (#7)
    Co-authored-by: Robert A <robert.ayetey@turntabl.io>

--- hackproject/code/src/app.ts
+++ hackproject/code/src/app.ts
@@ -1,6 +1,14 @@
 import path from "path";
 import { JSDOM } from "jsdom";
-import { rootPath, file, rgbToHex, validate, getFileLocation, FileType } from "./utils";
+import {
+  rootPath,
+  file,
+  rgbToHex,
+  validate,
+  getFileLocation,
+  FileType,
+  getCss,
+} from "./utils";
 import { DEFAULT_BACKGROUND, DEFAULT_FOREGROUND } from "./utils";
 import { FileNotSpecified, InvalidFile } from "./errors";
 
@@ -11,36 +19,40 @@ const main = async () => {
       htmlFile = getFileLocation(FileType.HTML);
     }
     const html = await file.read(path.join(rootPath, htmlFile));
-  
+
     let css = "";
     if (process.argv.includes("--css")) {
       const cssFile = getFileLocation(FileType.CSS);
       css = await file.read(path.join(rootPath, cssFile));
     }
-  
+
+    css += await getCss(html);
+
     const dom = new JSDOM(html, {
       runScripts: "dangerously",
       resources: "usable",
     });
-  
+
     dom.window.document.head.innerHTML += `<style>${css}</style>`;
-  
+
     dom.window.onload = () => {
       const body = dom.window.document.querySelector("body");
-      const bodyBgColor = rgbToHex(dom.window.getComputedStyle(body).backgroundColor) || DEFAULT_BACKGROUND;
-      const bodyColor = rgbToHex(dom.window.getComputedStyle(body).color) || DEFAULT_FOREGROUND;
-  
+      const bodyBgColor =
+        rgbToHex(dom.window.getComputedStyle(body).backgroundColor) ||
+        DEFAULT_BACKGROUND;
+      const bodyColor =
+        rgbToHex(dom.window.getComputedStyle(body).color) || DEFAULT_FOREGROUND;
+
       if (validate(dom, body, bodyBgColor, bodyColor)) {
         console.log("\x1b[32m accessibility test passed!\x1b[0m");
       } else {
         console.log("\x1b[31m accessibility test failed!\x1b[0m");
-      }    
+      }
     };
   } catch (error) {
     if (error instanceof FileNotSpecified) {
       console.error(error.message);
-    } 
-    else if (error instanceof InvalidFile) {
+    } else if (error instanceof InvalidFile) {
       console.error(error.message);
     } else {
       console.error(error);
--- hackproject/code/src/utils/index.ts
+++ hackproject/code/src/utils/index.ts
@@ -1,5 +1,10 @@
 export { default as file } from "./file";
 export { default as rootPath } from "./srcPath";
-export { default as rgbToHex, DEFAULT_BACKGROUND, DEFAULT_FOREGROUND } from "./color";
+export {
+  default as rgbToHex,
+  DEFAULT_BACKGROUND,
+  DEFAULT_FOREGROUND,
+} from "./color";
 export { validate } from "./contrast";
 export { FileType, getFileLocation } from "./argument";
+export { default as getCss } from "./getCss";
