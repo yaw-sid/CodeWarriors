2b55d579bed06e4c95f9c8a5585d73848c2b4df5
Robert A <robert.ayetey@turntabl.io>
2023-05-05 19:08:33 +0000

    Migrate to commonjs

--- hackproject/code/src/utils/srcPath.ts
+++ hackproject/code/src/utils/srcPath.ts
@@ -1,5 +1,2 @@
 import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-export const rootPath = path.join(path.dirname(__filename), "../../");
+export const rootPath = path.join(path.dirname(__dirname), "../");
