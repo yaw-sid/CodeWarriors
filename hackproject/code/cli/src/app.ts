13a6309484c5b235058b5ebcd56a32537c59ea9c
Robert A <robert.ayetey@turntabl.io>
2023-05-08 08:46:12 +0000

    Add extension and remove .vscode from .gitignore

---
+++ hackproject/code/cli/src/app.ts
@@ -0,0 +1,53 @@
+import path from "path";
+import fs from "fs/promises";
+import { FileNotSpecified, InvalidFile } from "./errors";
+import { getFileLocation, FileType } from "./utils/argument";
+import { ContrastValidator, Requirement, validate } from "../../sdk/index";
+
+const main = async () => {
+  try {
+    let htmlFile = "index.html";
+    if (process.argv.includes("--html")) {
+      htmlFile = getFileLocation(FileType.HTML);
+    }
+    const htmlPath = path.join(__dirname, htmlFile);
+    const html = await fs.readFile(htmlPath, { encoding: "utf-8" });
+
+    let css = "";
+    if (process.argv.includes("--css")) {
+      const cssFile = getFileLocation(FileType.CSS);
+      css = await fs.readFile(path.join(__dirname, cssFile), {
+        encoding: "utf-8",
+      });
+    }
+
+    const responses = await validate({
+      html: html,
+      htmlPath: htmlPath,
+      validator: new ContrastValidator(),
+      requirement: Requirement.AA,
+    });
+
+    let isValid = true;
+
+    responses.forEach((response) => (isValid &&= response.isValid));
+
+    if (!isValid) {
+      console.log("\x1b[31m accessibility test failed!\x1b[0m");
+      process.exit(1);
+    }
+    console.log("\x1b[32m accessibility test passed!\x1b[0m");
+    process.exit(0);
+  } catch (error) {
+    if (error instanceof FileNotSpecified) {
+      console.error(error.message);
+    } else if (error instanceof InvalidFile) {
+      console.error(error.message);
+    } else {
+      console.error(error);
+    }
+    process.exit(1);
+  }
+};
+
+main().catch(console.error);
---
+++ hackproject/code/vscode-extension/src/extension.ts
@@ -0,0 +1,70 @@
+import * as vscode from "vscode";
+import * as sdk from "../../sdk";
+import { Diagnostic, Range, DiagnosticSeverity, TextDocument } from "vscode";
+
+export function activate(context: vscode.ExtensionContext) {
+  console.log('Congratulations, your extension "accessify" is now active!');
+
+  let disposable = vscode.commands.registerCommand("accessify.validate", () => {
+    const activeEditor = vscode.window.activeTextEditor;
+    if (!activeEditor) {
+      return;
+    }
+
+    const activeDocument = activeEditor.document;
+    if (activeDocument.languageId !== "html") {
+      vscode.window.showErrorMessage(
+        "accessify extension only works with HTML files"
+      );
+      return;
+    }
+
+    const decorationType = vscode.window.createTextEditorDecorationType({
+      backgroundColor: "red",
+    });
+
+    const updateDiagnostics = async (document: TextDocument): Promise<void> => {
+      const diagnostics: Diagnostic[] = [];
+
+      const responses = await sdk.validate({
+        html: document.getText(),
+        validator: new sdk.ComboValidator(),
+        requirement: sdk.Requirement.AA,
+        htmlPath: document.uri.path,
+      });
+
+      responses.forEach((response) => {
+        if (!response.isValid) {
+          response.errors.forEach((e) => {
+            const { start, end, error } = e;
+
+            diagnostics.push(
+              new Diagnostic(
+                new Range(document.positionAt(start), document.positionAt(end)),
+                error.message,
+                DiagnosticSeverity.Error
+              )
+            );
+          });
+        }
+      });
+
+      console.log(responses);
+
+      activeEditor.setDecorations(decorationType, diagnostics);
+    };
+
+    updateDiagnostics(activeDocument);
+
+    vscode.workspace.onDidChangeTextDocument((e) => {
+      if (e.document === activeDocument) {
+        updateDiagnostics(activeDocument);
+      }
+    });
+  });
+
+  context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
