563d6173592f8263af12701d722051d199cf4dec
Selorm <selormlotsu0@gmail.com>
2023-05-10 18:19:43 +0000

    fix conflicts

--- hackproject/code/src/utils/srcPath.ts
+++
@@ -1,7 +0,0 @@
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const srcPath = path.join(path.dirname(__filename), "../");
-
-export default srcPath;
