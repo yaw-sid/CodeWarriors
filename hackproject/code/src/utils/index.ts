d7205714bfeb9d51f60d5f81e1aec550880d3d27
Selorm <selormlotsu0@gmail.com>
2023-05-10 17:51:57 +0000

    fix merge conflicts

--- hackproject/code/src/utils/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as file } from "./file";
-export { default as srcPath } from "./srcPath";
-export { default as rgbToHex, DEFAULT_BACKGROUND, DEFAULT_FOREGROUND } from "./color";
-export { validate } from "./contrast";
